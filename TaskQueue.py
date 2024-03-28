d9a8d545622088ace7a6a377a9a13fd4bc2ef549
ilya orlov <iorlov@gmail.com>
2024-03-28 15:27:02 +0300

    - added queue implementation interface as abc class - moved default queue implementation to ListQueue class

--- TaskQueue.py
+++ TaskQueue.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-
+from abc import ABC, abstractmethod
 
 @dataclass(eq=True, order=True, frozen=True)
 class Resources:
@@ -16,25 +16,48 @@ class Task:
     content: str
     result: str
 
+
 class DuplicateIdException(Exception):
     pass
 
 
-class TaskQueue:
+class QueueImplementationBase(ABC):
+
+    @abstractmethod
+    def get_queue_length(self) -> int:
+        pass
+
+    @abstractmethod
+    def add_task(self, task: Task) -> None:
+        pass
+
+    @abstractmethod
+    def is_task_already_added(self, task: Task) -> bool:
+        pass
+
+    @abstractmethod
+    def get_task(self, available_resources: Resources) -> [Task, None]:
+        pass
+
+
+class ListQueue(QueueImplementationBase):
 
     def __init__(self):
         self._queue = []
 
-    def get_queue_length(self):
+    def get_queue_length(self) -> int:
         return len(self._queue)
 
-    def add_task(self, task: Task):
-
-        for t in self._queue:
-            if t.id == task.id:
-                raise DuplicateIdException(f"duplicate id {task.id} found in add_task")
+    def add_task(self, task: Task) -> None:
 
         self._queue.append(task)
+
+    def is_task_already_added(self, task: Task) -> bool:
+        for t in self._queue:
+            if t.id == task.id:
+                return True
+
+        return False
 
     def get_task(self, available_resources: Resources) -> [Task, None]:
         if not self._queue:
@@ -47,3 +70,22 @@ class TaskQueue:
                 return t
 
         return None
+
+
+class TaskQueue:
+
+    def __init__(self):
+        self._queue = ListQueue()
+
+    def get_queue_length(self) -> int:
+        return self._queue.get_queue_length()
+
+    def add_task(self, task: Task) -> None:
+
+        if self._queue.is_task_already_added(task):
+            raise DuplicateIdException(f"duplicate id {task.id} found in add_task")
+
+        self._queue.add_task(task)
+
+    def get_task(self, available_resources: Resources) -> [Task, None]:
+        return self._queue.get_task(available_resources)
